b3795beff633870ce50736e56b2b936a77f2622a
Theodor <54949643+theodorerlax@users.noreply.github.com>
2019-12-27 10:38:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 function loadData() {
     return new Promise((resolve, reject) => {
       setTimeout(resolve, 2000);
@@ -12,3 +14,14 @@ function loadData() {
     });
 
 
+
+
+    function animatedNavigate() {
+        $('a[href^="#"]').click(function(){ //берем все ссылки атрибут href которых начинается с #
+            if(document.getElementById($(this).attr('href').substr(1)) != null) { //на странице есть элемент с нужным нам id
+               $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500); // анимируем скролл к элементу
+            }     
+            return false;
+          });
+      }
+      
